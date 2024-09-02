430a71893dc2cf041c57e7c8d891c711b16890f5
supriadi <supriadi96@gmail.com>
2024-09-02 16:23:04 +0700

    add popular and pass input variable to html template

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import requests
+from bs4 import BeautifulSoup
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,8 +8,19 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/berita-popular')
+def berita_popular():
+    html_doc = requests.get('https://www.detik.com/terpopuler')
 
+    soup = BeautifulSoup(html_doc.text, 'html.parser')
 
+    popular_area = soup.find(attrs={'class':'grid-row list-content'})
+
+    titles = popular_area.find_all(attrs={'class':'media__title'})
+    images = popular_area.find_all(attrs={'class':'media__image'})
+
+    # call html file with argumen gambar=images
+    return render_template('index.html', gambars=images)
 
 if __name__ == '__main__':
     app.run(debug=True)
