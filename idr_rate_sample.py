a9f35453c081ca5885228d03900aaf106f9e49b0
supriadi <supriadi96@gmail.com>
2024-09-04 14:43:23 +0700

    using values

--- idr_rate_sample.py
+++ idr_rate_sample.py
@@ -4,4 +4,9 @@ source = requests.get('https://www.floatrates.com/daily/idr.json')
 #print(source.status_code)
 
 json_data = source.json()
-print(json_data)
+# print(json_data)
+for data in json_data.values():
+    print(data['code'])
+    print(data['name'])
+    print(data['date'])
+    print(data['inverseRate'])
