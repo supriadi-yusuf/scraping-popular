faeafd6f08e74ed20ba76f7c69ef500423840b03
supriadi <supriadi96@gmail.com>
2024-09-04 14:45:14 +0700

    using base.html for home page

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('base.html')
 
 @app.route('/berita-popular')
 def berita_popular():
